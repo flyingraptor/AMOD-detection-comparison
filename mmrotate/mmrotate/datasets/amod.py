35e8f6512904e7b39ef3777c82f74992a433329a
unique-chan <yechankim@gm.gist.ac.kr>
2025-09-07 01:30:53 +0900

    +

--- mmrotate/mmrotate/datasets/amod.py
+++ mmrotate/mmrotate/datasets/amod.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from itertools import chain
 from typing import List, Optional
+import os
 
 import numpy as np
 import pandas as pd
@@ -86,6 +87,9 @@ class AMODDataset(CustomDataset): # Add to __init__.py!
                 try:
                     if self.label_prefix is not None:  # For v1.5 label
                         # self.label_prefix? 'train_label_v1.5' or 'test_label_v1.5'!
+                        if os.path.exists(f'{self.data_root}{self.label_prefix}/Refined-{self.modality}_{sample_idx}_{angle}.csv'):
+                            print('NO exist: ', end='')
+                            print(f'{self.data_root}{self.label_prefix}/Refined-{self.modality}_{sample_idx}_{angle}.csv')
                         annot_df = pd.read_csv(
                             f'{self.data_root}{self.label_prefix}/Refined-{self.modality}_{sample_idx}_{angle}.csv'
                         )
