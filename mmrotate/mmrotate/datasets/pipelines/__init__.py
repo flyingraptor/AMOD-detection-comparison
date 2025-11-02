2ded6056ff0ab57b53bfb61014d082ca804eb969
unique-chan <yechankim@gm.gist.ac.kr>
2025-11-03 00:21:31 +0900

    +

--- mmrotate/mmrotate/datasets/pipelines/__init__.py
+++ mmrotate/mmrotate/datasets/pipelines/__init__.py
@@ -1,12 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .loading import LoadPatchFromImage , LoadAMODMasks
-from .transforms import PolyRandomRotate, RMosaic, RRandomFlip, RResize 
-
-#, MaskBrightness, ToGray
+from .loading import LoadPatchFromImage, LoadAMODMasks
+from .transforms import PolyRandomRotate, RMosaic, RRandomFlip, RResize, MaskBrightness, MaskContourBlur
 
 
 __all__ = [
-    'LoadPatchFromImage', 'RResize', 'RRandomFlip', 'PolyRandomRotate',
-    'RMosaic',]
-
-# 'MaskBrightness','ToGray','LoadAMODMasks']
+    'LoadPatchFromImage', 'LoadAMODMasks', 
+    'RResize', 'RRandomFlip', 'PolyRandomRotate',
+    'RMosaic', 'MaskBrightness', 'MaskContourBlur',
+]
