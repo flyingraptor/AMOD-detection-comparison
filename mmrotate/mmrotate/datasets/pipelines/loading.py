f3b53d9a41b1651b05bd81b1fb6ea8e15d010a96
unique-chan <yechankim@gm.gist.ac.kr>
2025-11-03 14:45:21 +0900

    +

--- mmrotate/mmrotate/datasets/pipelines/loading.py
+++ mmrotate/mmrotate/datasets/pipelines/loading.py
@@ -74,7 +74,7 @@ class LoadSegmentMasks:
 
 
 @ROTATED_PIPELINES.register_module()
-class LoadAMODMasks:
+class LoadSegmentMasks:
     """Load segmentation masks for AMOD dataset from .npy files.
     
     Each mask file is named 'Mask-{img_name}.npy'
